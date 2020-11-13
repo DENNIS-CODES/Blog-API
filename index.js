7f63703bca1df9195fb0ec8a9f531ef147c8acbd
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-11-14 00:35:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const mongoose = require("mongoose");
-const mongoDB = process.env.MONGO_URL || process.env.MONGO_URI;
 
-mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
