72bfa455e00d96e7f8c3dbaf262e75cbd7729dd5
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-11-15 00:06:06 +0000

    index.js and db.js

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const user = require("./user");
+const blog = require("./blog");
+const comment = require("./comment");
+const auth = require("./auth");
+
+module.exports = { user, blog, comment, auth };
